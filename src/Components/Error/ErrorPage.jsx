2da6dbc93a32526c85f093a89cecdef70501c526
hasib1010 <asifmahmud664@gmail.com>
2024-09-03 12:43:50 +0600

    retouch

--- src/Components/Error/ErrorPage.jsx
+++ src/Components/Error/ErrorPage.jsx
@@ -16,17 +16,21 @@ export default function ErrorPage() {
   return (
     <div id="error-page" className="flex flex-col min-h-screen bg-[#FAFAFB]">
       <Navbar2 />
+      <div className="container mx-auto">
+        <button
+          onClick={handleGoBack}
+          className="btn w-fit  btn-error px-6 py-2 lg:text-lg font-medium text-black transition-colors"
+        >
+          Go Back
+        </button>
+      </div>
       <div className="flex items-center p-5">
 
+
         <div className="flex-grow flex flex-col flex-1 items-center justify-center text-center">
-          <h2 className="lg:text-[233.239px] text-2xl font-medium text-[#EEEFF1]">404</h2> 
+          <h2 className="lg:text-[233.239px] text-2xl font-medium text-[#EEEFF1]">404</h2>
           <p className="lg:text-5xl font-medium mb-6">Oops...page not found</p>
-          <button
-            onClick={handleGoBack}
-            className="btn btn-error px-6 py-2 lg:text-lg font-medium text-black transition-colors"
-          >
-            Go Back
-          </button>
+
         </div>
         <img className="flex-1 w-1/2 h-1/2 rounded-xl " src={errorImg} alt="" />
       </div>
